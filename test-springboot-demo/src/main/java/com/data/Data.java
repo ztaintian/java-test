90f72f2da2c0897404ae19b715953441fde6a8fc
ztiantian <243752773@qq.com>
2017-09-15 17:10:48 +0800

    add

---
+++ test-springboot-demo/src/main/java/com/data/Data.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.data;
+public class Data{
+	private String nameId;
+
+	public String getNameId() {
+		return nameId;
+	}
+
+	public void setNameId(String nameId) {
+		this.nameId = nameId;
+	}
+}
--- test-springboot-demo/src/main/java/com/diditest/HelloController.java
+++ test-springboot-demo/src/main/java/com/diditest/HelloController.java
@@ -8,12 +8,13 @@
 package com.diditest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	 @RequestMapping("/hello1")
-	 public String index() {
-		 return "Hello World";
+	 public void index(@RequestParam("nameId") String nameId) {
+		 System.out.println(nameId);
 	 }
 }
