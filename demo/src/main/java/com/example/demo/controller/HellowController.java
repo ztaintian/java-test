ca42f5182eaa29a2c3ac97e272841997ff26fadf
unknown <zhutiantian@ciicsh.local>
2019-03-29 15:54:04 +0800

    hh

---
+++ demo/src/main/java/com/example/demo/controller/HellowController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HellowController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
